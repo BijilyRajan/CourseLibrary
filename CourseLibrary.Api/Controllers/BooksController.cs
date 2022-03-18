9deccecdf6900b90d9d1cdd26d88801078fe0a71
Bijily <bijily.rajan@teranet.ca>
2022-03-18 08:41:55 +0530

    Add project files.

---
+++ CourseLibrary.Api/Controllers/BooksController.cs
@@ -0,0 +1,49 @@
+﻿using CourseLibrary.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CourseLibrary.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : Controller
+    {
+
+
+        private readonly BooksDB _myBooksDB;
+        public BooksController(BooksDB myBooksDB)
+        {
+            _myBooksDB = myBooksDB;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            var books = await _myBooksDB.Books.ToListAsync();
+            return Ok(books);
+        }
+
+        [HttpGet]
+        [Route("get-book-by-id")]
+        public async Task<IActionResult> GetBooksByIdAsync(int id)
+        {
+            var book = await _myBooksDB.Books.FindAsync(id);
+            return Ok(book);
+        }
+        [HttpPost]
+        public async Task<IActionResult> PostAsync(Books books)
+        {
+            _myBooksDB.Books.Add(books);
+            await _myBooksDB.SaveChangesAsync();
+            return Created($"/get-book-by-id?id={books.BookID}", books);
+
+        }
+        [HttpPut]
+        public async Task<IActionResult> PutAync(Books bookToUpdate)
+        {
+            _myBooksDB.Books.Update(bookToUpdate);
+            await _myBooksDB.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
---
+++ CourseLibrary.Api/Models/Books.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CourseLibrary.Api.Models
+{
+    public class Books
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [System.Text.Json.Serialization.JsonIgnore]
+        public int BookID { get; set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public long ISBN { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public short Price { get; set; }
+        public int AuthorID { get; set; }
+
+    }
+
+    public class BooksDB : DbContext
+    {
+        public BooksDB(DbContextOptions<BooksDB> options) : base(options)
+        {
+
+        }
+        public DbSet<Books> Books { get; set; }
+       
+
+    }
+}
